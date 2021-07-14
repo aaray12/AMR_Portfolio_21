97ff3a0705a1c8cdcc7f9254421a45bd5e1cbed6
Aaron Ray <aaray619@gmail.com>
2021-07-14 15:23:22 -0500

    routing and init layout

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Col, Row, Container} from 'react-bootstrap';
+
+
+function LandingPage() {
+    return (
+        <div >
+        <Row id = "lpRow">
+            <Col id = "lpRightCol" md={7}>
+            <div>
+                <h4>
+                    Aaron Ray
+                </h4>
+            </div>
+            </Col>
+            <Col md={5} style ={{backgroundColor:"#7E5920"}}>
+                <div id ="lpMockBrowser" >
+                    <div id = "lpMockBrowserLine">
+
+                    </div>
+                </div>
+            <div style = {{height:"3em"}} >
+        </div>
+            </Col>
+        </Row>
+        </div>
+    );
+}
+
+
+export default LandingPage;
