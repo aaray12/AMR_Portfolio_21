2de29e3f95932a4ac2d1fe99332061b4301b5dd0
Aaron Ray <aaray619@gmail.com>
2021-07-26 09:54:35 -0500

    landing buildout

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Image } from 'cloudinary-react';
 import { Col, Row, Container, Button } from 'react-bootstrap';
-import { hinge, bounceInRight, zoomOutDown, zoomIn, zoomInUp,zoomOut,
-        fadeInLeft, fadeInDown,fadeIn, flipInX, headShake, pulse, tada } from 'react-animations';
+import {
+    hinge, bounceInRight, zoomOutDown, zoomIn, zoomInUp, zoomOut,
+    fadeInLeft, fadeInDown, fadeIn, flipInX, headShake, pulse, tada
+} from 'react-animations';
 import { StyleSheet, css } from 'aphrodite';
 
 const me2 = "Portfolio/me2_wpbume"
@@ -28,8 +30,8 @@ function LandingPage() {
             animationName: bounceInRight,
             animationDuration: '1.5s'
         },
-       pulse: {
-            animationName:pulse,
+        pulse: {
+            animationName: pulse,
             animationDuration: '1s'
         },
         zoomOutDown: {
@@ -68,7 +70,7 @@ function LandingPage() {
     function MockBrowserClose(e) {
         e.preventDefault();
         document.getElementById("lpMockBrowser").style.display = "none"
-        function showSkyline(){
+        function showSkyline() {
             document.getElementById("lpChiSkyline").style.visibility = "visible"
             document.getElementById("lpChiSkyline").classList.add(css(styles.fadeIn))
 
@@ -81,10 +83,13 @@ function LandingPage() {
         function WaitToClose() {
             document.getElementById("lpMockBrowser").style.display = "none"
         }
-        function showSkyline(){
+        function showSkyline() {
             document.getElementById("lpChiSkyline").style.visibility = "visible"
             document.getElementById("lpChiSkyline").classList.add(css(styles.fadeIn))
-
+            document.getElementById("lpBrowserLinks2").style.visibility = "visible"
+            document.getElementById("lpBrowserLinks2").classList.add(css(styles.fadeIn))
+            document.getElementById("lpChiBorder").style.visibility = "visible"
+            document.getElementById("lpChiBorder").classList.add(css(styles.fadeIn))
         }
         setTimeout(WaitToClose, 1000);
         setTimeout(showSkyline, 2000);
@@ -108,19 +113,19 @@ function LandingPage() {
             myImage.src = "../images/me1.png"
         })
     }
-    function showAboutBtn(){
+    function showAboutBtn() {
         document.getElementById('lpAboutBtn').style.visibility = "visible"
         document.getElementById('lpAboutBtn').classList.add(css(styles.zoomInUp))
     }
-    function showProjectsBtn(){
+    function showProjectsBtn() {
         document.getElementById('lpProjectsBtn').style.visibility = "visible"
         document.getElementById('lpProjectsBtn').classList.add(css(styles.zoomInUp))
     }
-    function showContactBtn(){
+    function showContactBtn() {
         document.getElementById('lpContactBtn').style.visibility = "visible"
         document.getElementById('lpContactBtn').classList.add(css(styles.zoomInUp))
     }
-    function showBlogBtn(){
+    function showBlogBtn() {
         document.getElementById('lpBlogBtn').style.visibility = "visible"
         document.getElementById('lpBlogBtn').classList.add(css(styles.zoomInUp))
     }
@@ -137,24 +142,24 @@ function LandingPage() {
     setTimeout(showBlogBtn, 2500);
     setTimeout(showContactBtn, 2750);
 
-    function heyShake(){
+    function heyShake() {
         document.getElementById('lpHey').classList.add(css(styles.headShake))
     }
-    function heyShakeOut(){
+    function heyShakeOut() {
         document.getElementById('lpHey').classList.remove(css(styles.headShake))
     }
-    function imAaBounce(){
+    function imAaBounce() {
         document.getElementById('lpImAaron').classList.add(css(styles.pulse))
     }
-    function imAaBounceOut(){
+    function imAaBounceOut() {
         document.getElementById('lpImAaron').classList.remove(css(styles.pulse))
     }
-    function fsdZooms(){
+    function fsdZooms() {
         document.getElementById('aFSDeveloper').classList.add(css(styles.tada))
 
 
     }
-    function fsdZoomsOut(){
+    function fsdZoomsOut() {
         document.getElementById('aFSDeveloper').classList.remove(css(styles.tada))
     }
     return (
@@ -167,22 +172,29 @@ function LandingPage() {
                         <h5 id="lpAR" >AR</h5>
                     </div>
                     <div id="lpTextDiv" className={css(styles.fadeInLeft)}>
-                        <h2 className = "lpLeftHeadings">
-                            <div id = "lpHey" onMouseOver = { heyShake} onMouseOut = {heyShakeOut} ><span id = "hInHey">H</span>ey,</div>
+                        <h2 className="lpLeftHeadings">
+                            <div id="lpHey" onMouseOver={heyShake} onMouseOut={heyShakeOut} ><span id="hInHey">H</span>ey,</div>
                         </h2>
-                        <h2 className = "lpLeftHeadings">
-                           <div id = "lpImAaron" onMouseOver = { imAaBounce} onMouseOut = {imAaBounceOut}>I'm Aaron,</div>
+                        <h2 className="lpLeftHeadings">
+                            <div id="lpImAaron" onMouseOver={imAaBounce} onMouseOut={imAaBounceOut}>I'm Aaron,</div>
                         </h2>
-                        <h2 className = "lpLeftHeadings">
-                           <div id = "aFSDeveloper" onMouseOver = { fsdZooms} onMouseOut = {fsdZoomsOut}><span id = "fsdA">a</span> <span id = "fsdFull">full</span><span id = "fsd-">-</span><span id = "fsdStack">stack</span> <span id = "fsdWeb">web</span> <span id = "fsdDev">developer</span><span id = "fsdPeriod">.</span></div> 
+                        <h2 className="lpLeftHeadings">
+                            <div id="aFSDeveloper" onMouseOver={fsdZooms} onMouseOut={fsdZoomsOut}><span id="fsdA">a</span> <span id="fsdFull">full</span><span id="fsd-">-</span><span id="fsdStack">stack</span> <span id="fsdWeb">web</span> <span id="fsdDev">developer</span><span id="fsdPeriod">.</span></div>
                         </h2>
-                        <p id = "lpTechLine"><span className = "lpTechList">JaveScript</span> / <span className = "lpTechList">React</span> / <span className = "lpTechList">NodeJS</span> / <span className = "lpTechList">Bootstrap</span> / <span className = "lpTechList">WordPress</span></p>
+                        <p id="lpTechLine"><span className="lpTechList">JaveScript</span> / <span className="lpTechList">React</span> / <span className="lpTechList">NodeJS</span> / <span className="lpTechList">Bootstrap</span> / <span className="lpTechList">WordPress</span></p>
                     </div>
                 </Col>
                 <Col md={5} style={{ backgroundColor: "#7E5920" }}>
-                <div id = "lpChiDiv">
-                            <img style = {{visibility: "hidden"}} id = "lpChiSkyline" src = {chiLandscape}></img>
-                        </div>
+                    <div id="lpChiDiv">
+                        <img style={{ visibility: "hidden" }} id="lpChiSkyline" src={chiLandscape}></img>
+                        <div id = "lpChiBorder" ></div>
+                        <div id="lpBrowserLinks2" style = {{visibility: "hidden"}}>
+                        <Button className="navBtns2" id = "lpAboutBtn2">About</Button>
+                        <Button className="navBtns2" id= "lpProjectsBtn2">Projects</Button>
+                        <Button className="navBtns2" id= "lpBlogBtn2">Blog</Button>
+                        <Button className="navBtns2" id= "lpContactBtn2">Contact</Button>
+                    </div>
+                    </div>
                     <div id="lpMockBrowser" className={css(styles.bounceInRight)} >
                         <div id="lpMockBrowserLine">
                             <div id="lpMockBrowserButtonsDiv">
@@ -200,11 +212,11 @@ function LandingPage() {
                         <div id="lpMeImgDiv">
                             <img id="me1" src="../images/me1.png" style={{ visibility: "hidden" }}></img>
                         </div>
-                        <div id ="lpBrowserLinks">
-                            <Button className="navBtns" id = "lpAboutBtn">About</Button>
-                            <Button className="navBtns" id = "lpProjectsBtn">Projects</Button>
-                            <Button className="navBtns" id = "lpBlogBtn">Blog</Button>
-                            <Button className="navBtns" id = "lpContactBtn">Contact</Button>
+                        <div id="lpBrowserLinks">
+                            <Button className="navBtns" id="lpAboutBtn">About</Button>
+                            <Button className="navBtns" id="lpProjectsBtn">Projects</Button>
+                            <Button className="navBtns" id="lpBlogBtn">Blog</Button>
+                            <Button className="navBtns" id="lpContactBtn">Contact</Button>
                         </div>
                         <div id="lpBrowsrerBackground">
 
